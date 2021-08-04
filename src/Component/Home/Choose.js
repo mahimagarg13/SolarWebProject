f76a328e25622828ed9cec1886d381aee676ab87
Mahima garg <mahimagarg832@gmail.com>
2021-08-04 12:45:53 +0530

    dd

--- src/Component/Home/Choose.js
+++ src/Component/Home/Choose.js
@@ -20,46 +20,47 @@ function Choose() {
 					<div className="splide__track">
 						<ul className="splide__list flexWrap">
 							<li className="splide__slide m-right">
-								<div className="card cardContainerYellow m-b ">
+								<div className="card cardContainerYellow m-b  text-center">
 									<div className="card-body  m-left">
-										<img className="h-w" src=""  alt="img"/>
+										<img className="h-w" src="/img/1p.jpeg"  alt="img"/>
 										<h6 className="color">  Ajay S.</h6>
 										<p className="pd">"Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply text of the printing and typesetting industry text of the printing and typesetting industry." </p>
 
 									</div></div>
 							</li>
-							<li className="splide__slide m-right"><div className="card cardContainerYellow m-b">
+							<li className="splide__slide m-right"><div className="card cardContainerYellow text-center m-b">
 								<div className="card-body">
-									<img className="h-w" src="" alt="img" />
+									<img className="h-w" src="/img/2p.png" alt="img" />
 									<h6 className="color">
 										Akshay S.</h6>
 									<p className="pd">"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry."</p>
 
 								</div></div></li>
-							<li className="splide__slide m-right"><div className="card cardContainerYellow m-b">
+							<li className="splide__slide m-right"><div className="card cardContainerYellow text-center m-b">
 								<div className="card-body">
-									<img className="h-w" src="" alt="img"/>
+									<img className="h-w" src="/img/3p.jpeg" alt="img"/>
 									<h6 className="color">Robert Anl</h6>
 									<p className="pd">Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
 
 								</div></div></li>
-							<li className="splide__slide m-right"><div className="card cardContainerYellow m-b">
+							<li className="splide__slide m-right">
+								<div className="card cardContainerYellow  text-center m-b">
 								<div className="card-body">
-									<img className="h-w" src="" alt="img" />
+									<img className="h-w" src="/img/1p.jpeg" alt="img" />
 									<h6 className="color">  deniel Gil</h6>
 									<p className="pd">"Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry." </p>
 
 								</div></div></li>
-							<li className="splide__slide m-right"><div className="card cardContainerYellow m-b">
+							<li className="splide__slide m-right"><div className="card text-center cardContainerYellow m-b">
 								<div className="card-body">
-									<img className="h-w" src="" alt="img"/>
+									<img className="h-w" src="/img/2p.png" alt="img"/>
 									<h6 className="color">Sam R.</h6>
 									<p className="pd">"Lorem Ipsum is simply dummy text of the printing and typesetting.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry." </p>
 
 								</div></div></li>
-							<li className="splide__slide m-right"><div className="card cardContainerYellow m-b">
+							<li className="splide__slide m-right"><div className="card text-center cardContainerYellow m-b">
 								<div className="card-body">
-									<img className="h-w" src="" alt="img" />
+									<img className="h-w" src="/img/3p.jpeg" alt="img" />
 									<h6 className="color">  Neel Jons</h6>
 									<p className="pd">"Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum is simply dummy text of the printing and typesetting industry." </p>
 
